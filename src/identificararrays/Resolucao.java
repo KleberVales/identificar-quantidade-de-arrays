540e42cb26e16051898772d121c6c59176412e4c
Kleber Vales <ksvales@outlook.pt>
2024-10-08 09:14:55 -0300

    resolvendo problema da identação

--- src/identificararrays/Resolucao.java
+++ src/identificararrays/Resolucao.java
@@ -7,16 +7,18 @@ import java.util.stream.Collectors;
 
 public class Resolucao {
 
-    public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
-       scan.nextLine();
-       int count = 0;
-       List<Integer> A = Arrays.stream(scan.nextLine().trim().split("\\s+"))
-                    .map(Integer::parseInt).collect(Collectors.toList());
-       for (int i = 0; i < A.size(); i++)
-           {for (int j = i+1; j <= A.size(); j++)
-               {if (A.subList(i, j).stream()
-               .mapToInt(Integer::intValue).sum() < 0) count++;}}
-       System.out.println(count);
-       }
-    }
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+		int count = 0;
+		List<Integer> A = Arrays.stream(scan.nextLine().trim().split("\\s+")).map(Integer::parseInt)
+				.collect(Collectors.toList());
+		for (int i = 0; i < A.size(); i++) {
+			for (int j = i + 1; j <= A.size(); j++) {
+				if (A.subList(i, j).stream().mapToInt(Integer::intValue).sum() < 0)
+					count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
